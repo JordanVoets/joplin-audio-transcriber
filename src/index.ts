9869528e5aca4f9f28f2bb9e8f846b6524f14aeb
Jordan Voets¨ <jordan.voets@gmail.com>
2026-02-13 14:08:16 +0100

    chore: Update startup log

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import joplin from 'api';
 
 joplin.plugins.register({
 	onStart: async function() {
-		// eslint-disable-next-line no-console
-		console.info('Hello world. Test plugin started!');
+		console.info('Joplin Audio Transcriber plugin started!');
 	},
 });
